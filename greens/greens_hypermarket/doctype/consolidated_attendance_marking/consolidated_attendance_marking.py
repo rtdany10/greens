fa9384c2030a6a3595ba178efa7d4f2fefdddf3d
Dany Robert <rtdany10@gmail.com>
2022-01-18 22:05:14 -0800

    fix: linters

--- greens/greens_hypermarket/doctype/consolidated_attendance_marking/consolidated_attendance_marking.py
+++ greens/greens_hypermarket/doctype/consolidated_attendance_marking/consolidated_attendance_marking.py
@@ -2,8 +2,9 @@
 # For license information, please see license.txt
 
 import frappe
-from frappe.utils import today
 from frappe.model.document import Document
+from frappe.utils import today
+
 
 class ConsolidatedAttendanceMarking(Document):
 	def before_insert(self):
