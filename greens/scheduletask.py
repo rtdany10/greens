59f85bcb627b2b62c4323ec8240b05c22e6e1582
Dany Robert <rtdany10@gmail.com>
2022-08-17 22:53:06 -0700

    fix: casual leave allocation

--- greens/scheduletask.py
+++ greens/scheduletask.py
@@ -1,21 +1,20 @@
 import frappe
-from frappe import _
-from frappe.utils import date_diff,getdate,today
-from frappe.utils.data import get_last_day,get_first_day
-from pypika.functions import DateDiff,CurDate
+from frappe.utils.data import get_first_day, get_last_day
+from pypika import CustomFunction
+from pypika.functions import CurDate
+
 
 def casual_leave_allocation():
 	employees = frappe.qb.DocType('Employee')
+	DateDiff = CustomFunction('DATEDIFF', ['start_date', 'end_date'])
 	emp_data = (
 		frappe.qb.from_(employees)
-			.select(employees.employee,employees.date_of_joining,employees.status)
-			.where(employees.status == 'Active') 
-			.where((DateDiff('day',CurDate(), employees.date_of_joining,alias=None) >= 365))
-			).run(as_dict=True)
+		.select(employees.employee)
+		.where(employees.status == 'Active')
+		.where((DateDiff(CurDate(), employees.date_of_joining) >= 365))
+		.run(as_dict=True)
+	)
 	for emp in emp_data:
-		#date_of_joining = frappe.db.get_value("Employee", emp, "date_of_joining")
-		#number_of_days = date_diff(frappe.utils.today(), date_of_joining)
-		#if number_of_days >=365:
 		allocation = frappe.get_doc(
 			dict(
 				doctype="Leave Allocation",
